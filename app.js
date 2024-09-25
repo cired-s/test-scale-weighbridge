4772104ab92b3c0c123df24c4fcb2cf5a624495d
cired-s <cirednus@gmail.com>
2024-09-25 12:04:20 +0800

    Update app.js

--- app.js
+++ app.js
@@ -15,17 +15,14 @@ fetch('scale-data.json')
     .then(response => response.json())
     .then(data => {
         data.forEach(item => {
-            // 在磅秤圖層中標記每個磅秤的位置
             const scaleMarker = L.marker([item.latitude, item.longitude]).addTo(scaleLayer);
-
-            // 為每個標記綁定 Popup，顯示磅秤資訊
             scaleMarker.bindPopup(`
                 <b>${item.店名}</b><br>
                 廠牌: ${item.廠牌}<br>
                 型式: ${item.型式}<br>
                 器號: ${item.器號}<br>
-                Max (kg): ${item.Max (kg)}<br>
-                e (g): ${item.e (g)}<br>
+                Max (kg): ${item.Max_kg}<br>
+                e (g): ${item.e_g}<br>
                 檢查日期: ${item.檢查日期}<br>
                 檢查合格與否: ${item.檢查合格與否}<br>
                 檢定日期: ${item.檢定日期}<br>
@@ -37,24 +34,20 @@ fetch('scale-data.json')
         console.error('Error loading the JSON file:', error);
     });
 
-// 添加地磅資訊到地磅圖層（可以是靜態資訊或來自另一個 API）
-// 從 weighbridge-data.json 讀取磅秤資料並在地圖上顯示
+// 從 weighbridge-data.json 讀取地磅資料並在地圖上顯示
 fetch('weighbridge-data.json')
     .then(response => response.json())
     .then(data => {
         data.forEach(item => {
-            // 在地磅圖層中標記每個地秤的位置
-            const scaleMarker = L.marker([item.latitude, item.longitude]).addTo(scaleLayer);
-
-            // 為每個標記綁定 Popup，顯示地磅資訊
-            scaleMarker.bindPopup(`
+            const storeMarker = L.marker([item.latitude, item.longitude]).addTo(storeLayer);
+            storeMarker.bindPopup(`
                 <b>${item.所有人}</b><br>
                 地址: ${item.地址}<br>
                 廠牌: ${item.廠牌}<br>
                 型號: ${item.型號}<br>
                 器號: ${item.器號}<br>
-                Max (t): ${item.Max (t)}<br>
-                e (kg): ${item.e (kg)}<br>
+                Max (t): ${item.Max_t}<br>
+                e (kg): ${item.e_kg}<br>
                 檢定日期: ${item.檢定日期}<br>
                 檢定合格單號: ${item.檢定合格單號}<br>
                 案號: ${item.案號}
@@ -64,6 +57,7 @@ fetch('weighbridge-data.json')
     .catch(error => {
         console.error('Error loading the JSON file:', error);
     });
+
 // 添加圖層控制，讓用戶可以選擇顯示哪些圖層
 const baseLayers = {};
 const overlays = {
